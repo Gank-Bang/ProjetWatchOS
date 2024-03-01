7ea931f8c979ddd5d1305d7273a578228f5aa4ae
Gank-Bang <messairafiq@gmail.com>
2024-03-01 14:20:04 +0100

    Initial Commit

---
+++ ProjetWatchOs Watch App/ProjetWatchOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetWatchOsApp.swift
+//  ProjetWatchOs Watch App
+//
+//  Created by Rafiq Messai on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjetWatchOs_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjetWatchOs/ProjetWatchOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetWatchOsApp.swift
+//  ProjetWatchOs
+//
+//  Created by Rafiq Messai on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjetWatchOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
